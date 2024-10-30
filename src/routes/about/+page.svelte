bdce30db9b488502f505fa5777215d066a583607
Kaze Wong <kazewong.physics@gmail.com>
2024-10-29 22:27:17 -0400

    Add interactive button to update name count

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,20 @@
 <script>
     let name = $state('world');
-  </script>
+
+    let count = $state(0);
+
+    function onclick() {
+    count += 1;
+    }
+</script>
   
-  <input bind:value={name} placeholder="Enter your name" />
-  {#if name === 'world'}
+<input bind:value={name} placeholder="Enter your name" />
+{#if name === 'world'}
   <h1>Hello {name}!</h1>
 {:else}
   <h1>I don't say hello to anyone else</h1>
 {/if}
+
+<button {onclick}>
+    Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
